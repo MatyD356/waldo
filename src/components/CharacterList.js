2c539512f4b20ce4551ba0539014c57706d97258
Mateusz <matyd356@gmail.com>
2020-09-28 15:17:22 +0200

    export list of characters to find to new component

---
+++ src/components/CharacterList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CharacterList = ({ characters }) => {
+  return (
+    <ul className='character-list'>
+      {characters.map(item => <li className='character-item' key={item.x}>
+        <img className='avatar' src={item.img} />
+        <span className={item.hit === false ? 'red' : 'green'}>{item.name}</span>
+      </li>)}
+    </ul >
+  )
+}
+export default CharacterList
