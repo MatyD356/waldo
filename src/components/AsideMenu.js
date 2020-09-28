1e8d0ec4ec3ede538c51534e955ea21d79f42f74
Mateusz <matyd356@gmail.com>
2020-09-28 15:18:30 +0200

    clean up AsideMenu component and add styles to it

--- src/components/AsideMenu.js
+++ src/components/AsideMenu.js
@@ -1,23 +1,17 @@
 import React from 'react'
-import '../styles/AsideMenu.scss'
+import Navigation from './Navigation'
+import CharacterList from './CharacterList'
 
+import '../styles/AsideMenu.scss'
 
 const AsideMenu = ({ characters }) => {
 
   return (
-    <aside className="AsideMenu">
-      <h1>Where's Waldo</h1>
-      <p>find Waldo and his friends</p>
-      <nav>
-        <ul className="nav-list">
-          <li>home</li>
-          <li>Score</li>
-          <li>Faq</li>
-        </ul>
-      </nav>
-      <ul className="character-list">
-        {characters.map(item => <li key={item.x}>{item.name}:{item.hit.toString()}</li>)}
-      </ul >
+    <aside className='AsideMenu'>
+      <h1 className='title'>Where's Waldo</h1>
+      <p className='sub-title'>find Waldo and his friends</p>
+      <Navigation />
+      <CharacterList characters={characters} />
     </aside >
   )
 }
