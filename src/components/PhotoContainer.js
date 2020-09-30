4212b7ae82599e9fafd2e29820b4f498c3638e87
Mateusz <matyd356@gmail.com>
2020-09-30 09:29:28 +0200

    add simple game controlls prototype

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -4,9 +4,9 @@ import Circle from './Circle'
 
 import { getPicture } from '../Firebase.js'
 
-const PhotoContainer = ({ characters, updateCharacters }) => {
+const PhotoContainer = ({ characters, updateCharacters, gameOn, updateGameOn }) => {
 
-  const [lastClick, setLastClick] = useState({ x: '', y: '' })
+  const [lastClick, setLastClick] = useState({ x: -1000, y: -1000 })
   const [currentImg, setcurrentImg] = useState('')
 
   //get data from database
@@ -34,23 +34,29 @@ const PhotoContainer = ({ characters, updateCharacters }) => {
       let hits = characters.filter(item => item.hit === true)
       if (hits.length === characters.length) {
         alert('u won')
+        updateGameOn(!gameOn)
       }
     }
   }, [JSON.stringify(characters)])
+
   const handleClick = (e) => {
     let newX = e.nativeEvent.offsetX
     let newY = e.nativeEvent.offsetY
     setLastClick({ x: newX, y: newY })
   }
   return (
-    <div
-      onClick={handleClick}
-      className='PhotoContainer'>
-      <img
-        className='waldoPhoto'
-        src={`${currentImg}`}
-        alt='waldo one'
-      />
+    <div onClick={handleClick} className='PhotoContainer'>
+      {/*extract this to new component*/}
+      {!gameOn ? <div className='game-controls'>
+        <button className='control-button' onClick={() => updateGameOn(!gameOn)}>Start</button>
+        <button className='control-button'>See scores</button>
+      </div> :
+        <img
+          className='waldoPhoto'
+          src={`${currentImg}`}
+          alt='waldo one'
+        />}
+
       {characters ? characters.map((item, index) => {
         if (item.hit) {
           return <Circle key={index} cords={{ x: item.x, y: item.y }} main={false} name={item.name} />
