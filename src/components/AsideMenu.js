50cf73348e92d4beec0831174ba6fda9f2dbe76e
Mateusz <matyd356@gmail.com>
2020-09-30 09:28:15 +0200

    add a basic time counter

--- src/components/AsideMenu.js
+++ src/components/AsideMenu.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Navigation from './Navigation'
 import CharacterList from './CharacterList'
 
 import '../styles/AsideMenu.scss'
 
-const AsideMenu = ({ characters }) => {
+const AsideMenu = ({ characters, gameOn }) => {
+  const [time, setTime] = useState(0)
+  useEffect(() => {
+    if (gameOn) {
+      const interval = setInterval(() => {
+        setTime(time => time + 1)
+      }, 1000)
+      return () => clearInterval(interval)
+    }
+  }, [gameOn])
 
   return (
     <aside className='AsideMenu'>
       <h1 className='title'>Where's Waldo</h1>
-      <p className='sub-title'>find Waldo and his friends</p>
       <Navigation />
+      <h2 className='sub-title'>Find Waldo and his friends</h2>
       <CharacterList characters={characters} />
+      <div>
+        <h2>Your time</h2>
+        <p>{time}</p>
+      </div>
     </aside >
   )
 }
