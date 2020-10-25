e57b8531fd1df455252679f02b2a13cfb6a8c873
Mateusz <matyd356@gmail.com>
2020-10-26 00:58:56 +0100

    Updates

---
+++ precache-manifest.f54d8814d9844f305bd22625be97337c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76cfb2b4c4f1a7ea0df11f923804c8c5",
+    "url": "/waldo/index.html"
+  },
+  {
+    "revision": "2d6e9cde78cd404c78d3",
+    "url": "/waldo/static/css/main.e02d2351.chunk.css"
+  },
+  {
+    "revision": "b7987f4789fdbdfa3c36",
+    "url": "/waldo/static/js/2.867f7b27.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/waldo/static/js/2.867f7b27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d6e9cde78cd404c78d3",
+    "url": "/waldo/static/js/main.11ea9d8f.chunk.js"
+  },
+  {
+    "revision": "2749c421917ef3420223",
+    "url": "/waldo/static/js/runtime-main.fbce9503.js"
+  }
+]);
